9eb37df034b9ad1f6d9e0765687d387072344159
Pedro Costa <costapedro.a1999@gmail.com>
2022-01-12 17:24:42 +0000

    leveraging previous work

--- app/controllers/spree/admin/parts_controller.rb
+++ app/controllers/spree/admin/parts_controller.rb
@@ -8,7 +8,7 @@ class Spree::Admin::PartsController < Spree::Admin::BaseController
   def remove
     assembly_part = Spree::AssembliesPart.find(params[:id])
     assembly_part.destroy
-    redirect_back fallback_location: admin_product_parts_url(product)
+    render 'spree/admin/parts/update_parts_table'
   end
 
   def set_count
@@ -37,7 +37,7 @@ class Spree::Admin::PartsController < Spree::Admin::BaseController
   def save_part(part_params)
     form = Spree::AssignPartToBundleForm.new(product, part_params)
     if form.submit
-      redirect_back fallback_location: admin_product_parts_url(product)
+      render 'spree/admin/parts/update_parts_table'
     else
       error_message = form.assemblies_part.errors.full_messages.to_sentence
       render json: error_message.to_json, status: 422
